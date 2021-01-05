0b8599e13a020e7fe0ecc6aa4e6ee4432a017348
hq <657473650@qq.com>
2021-01-05 16:00:12 +0800

    ^

--- src/main/java/top/kukechen/paperresourcebackend/controller/PaperController.java
+++ src/main/java/top/kukechen/paperresourcebackend/controller/PaperController.java
@@ -3,6 +3,7 @@ package top.kukechen.paperresourcebackend.controller;
 import com.mongodb.client.result.UpdateResult;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.data.domain.Sort;
 import org.springframework.data.mongodb.core.MongoTemplate;
 import org.springframework.data.mongodb.core.query.Criteria;
 import org.springframework.data.mongodb.core.query.Query;
@@ -14,9 +15,12 @@ import top.kukechen.paperresourcebackend.model.Paper;
 import top.kukechen.paperresourcebackend.restservice.Response;
 import top.kukechen.paperresourcebackend.restservice.ResponseWrap;
 import top.kukechen.paperresourcebackend.service.MongoDBUtil;
+import top.kukechen.paperresourcebackend.service.PageModel;
 import top.kukechen.paperresourcebackend.units.FileUpload;
 import top.kukechen.paperresourcebackend.units.PassToken;
+
 import java.io.File;
+import java.util.HashMap;
 import java.util.List;
 
 import static top.kukechen.paperresourcebackend.restservice.Response.STAUTS_FAILED;
@@ -44,13 +48,55 @@ public class PaperController {
         MongoTemplate mongoTemplate = MongoDBUtil.mongodbUtil.mongoTemplate;
         Query query = Query.query(Criteria.where("name").is(paper.getName()));
         Paper _paper = mongoTemplate.findOne(query, Paper.class);
-        if(_paper != null) {
+        if (_paper != null) {
             return new Response<Paper>(STAUTS_FAILED, "文件已存在");
         }
         mongoTemplate.save(paper);
         return new Response(0, paper);
     }
 
+    @PostMapping("/list")
+    @PassToken
+    public Response<Grade> getPaperList() {
+        MongoTemplate mongoTemplate = MongoDBUtil.mongodbUtil.mongoTemplate;
+        List<Paper> list = mongoTemplate.findAll(Paper.class, "paper");
+        return new Response(STAUTS_OK, list);
+    }
+
+    @PostMapping("/delete")
+    @PassToken
+    public Response<Grade> deletePaper(@RequestBody ResponseWrap rw) {
+        String id = rw.getId();
+        logger.info("开始删除试卷: " + id);
+        int result = MongoDBUtil.removeById(id, "paper");
+        logger.info("删除试卷成功: id为" + id + " 删除结果 - result: " + result);
+        if (result == 1) {
+            return new Response(STAUTS_OK, "删除成功");
+        } else {
+            return new Response(STAUTS_FAILED, "删除失败");
+        }
+    }
+
+
+    @PostMapping("/page")
+    @PassToken
+    public Response<Grade> getGradePage(@RequestBody ResponseWrap rw) {
+        HashMap query = new HashMap<String, Object>();
+        PageModel page = MongoDBUtil.findSortPageCondition(Paper.class, "paper", rw.getQuery(), rw.getPageNo(), rw.getPageSize(), Sort.Direction.ASC, "created");
+        return new Response(STAUTS_OK, page);
+    }
+
+    @PostMapping("/edit")
+    @PassToken
+    public Response<Paper> editGrade(@RequestBody Paper paper) {
+        MongoTemplate mongoTemplate = MongoDBUtil.mongodbUtil.mongoTemplate;
+        Criteria criteria = Criteria.where("_id").is(paper.getId());
+        Query query = Query.query(criteria);
+        Update update = new Update();
+        update.set("name", paper.getName());
+        UpdateResult res = mongoTemplate.updateFirst(query, update, Paper.class);
+        return new Response(0, res);
+    }
 
 
 //    @PostMapping("/edit")
