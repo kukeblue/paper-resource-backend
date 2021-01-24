8b8abb758522e727b0847a7ed25eb38b9b1967d5
GOGOHQ <657473650qq.com>
2021-01-24 22:53:33 +0800

    saveUser

--- src/main/java/top/kukechen/paperresourcebackend/controller/WxUserController.java
+++ src/main/java/top/kukechen/paperresourcebackend/controller/WxUserController.java
@@ -1,18 +1,24 @@
 package top.kukechen.paperresourcebackend.controller;
 
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.data.mongodb.core.MongoTemplate;
+import org.springframework.data.mongodb.core.query.Criteria;
+import org.springframework.data.mongodb.core.query.Query;
+import org.springframework.web.bind.annotation.*;
 import top.kukechen.paperresourcebackend.model.Paper;
+import top.kukechen.paperresourcebackend.model.Tag;
 import top.kukechen.paperresourcebackend.model.WxResponse;
 import top.kukechen.paperresourcebackend.model.WxUser;
 import top.kukechen.paperresourcebackend.restservice.Response;
+import top.kukechen.paperresourcebackend.restservice.ResponseWrap;
+import top.kukechen.paperresourcebackend.service.MongoDBUtil;
 import top.kukechen.paperresourcebackend.utils.HttpClientUtils;
 import top.kukechen.paperresourcebackend.utils.JsonUtils;
 import top.kukechen.paperresourcebackend.utils.PassToken;
 
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 import static top.kukechen.paperresourcebackend.restservice.Response.STAUTS_FAILED;
@@ -24,18 +30,13 @@ import static top.kukechen.paperresourcebackend.restservice.Response.STAUTS_OK;
 public class WxUserController {
     private  String appId = "wx4f594f42b6ca60d8";
     private String secret = "1a085e09f9e33c687ffe0ce6a031e3e6";
-
+    private static Logger logger = LoggerFactory.getLogger(WxUserController.class);
     @PassToken
     @RequestMapping("/login")
     @ResponseBody
     public Response<WxUser> login(String code)
     {
         System.out.println("wxlogin - code: " + code);
-//		https://api.weixin.qq.com/sns/jscode2session?
-//				appid=APPID&
-//				secret=SECRET&
-//				js_code=JSCODE&
-//				grant_type=authorization_code
         String url = "https://api.weixin.qq.com/sns/jscode2session";
         Map<String, String> param = new HashMap<>();
         param.put("appid", appId);
@@ -54,5 +55,37 @@ public class WxUserController {
         WxUser user  = new WxUser(res.getOpenid());
         return new Response<WxUser>(STAUTS_OK,user );
 
+    }
+    @PassToken
+    @PostMapping("/add")
+    public Response<WxUser> add(@RequestBody WxUser wxUser){
+        MongoTemplate mongoTemplate = MongoDBUtil.mongodbUtil.mongoTemplate;
+        Query query = Query.query(Criteria.where("nickName").is(wxUser.getNickName()));
+        WxUser _wxUser = mongoTemplate.findOne(query, WxUser.class);
+        if (_wxUser != null) {
+            return new Response<WxUser>(STAUTS_FAILED, "微信用户已存在");
+        }
+        mongoTemplate.save(wxUser);
+        return new Response(0, wxUser);
+    }
+    @PostMapping("/list")
+    @PassToken
+    public Response<Tag> getWxUserList() {
+        MongoTemplate mongoTemplate = MongoDBUtil.mongodbUtil.mongoTemplate;
+        List<WxUser> list = mongoTemplate.findAll(WxUser.class, "wx_user");
+        return new Response(STAUTS_OK, list);
+    }
+    @PostMapping("/delete")
+    @PassToken
+    public Response<Tag> deleteWxUser(@RequestBody ResponseWrap rw) {
+        String id = rw.getId();
+        logger.info("开始删除微信用户 " + id);
+        int result = MongoDBUtil.removeById(id, "wx_user");
+        logger.info("删除标签成功: id为" + id + " 删除结果 - result: " + result);
+        if (result == 1) {
+            return new Response(STAUTS_OK, "删除成功");
+        } else {
+            return new Response(STAUTS_FAILED, "删除失败");
+        }
     }
 }
--- src/main/java/top/kukechen/paperresourcebackend/model/WxUser.java
+++ src/main/java/top/kukechen/paperresourcebackend/model/WxUser.java
@@ -10,14 +10,18 @@ import org.springframework.data.mongodb.core.mapping.Document;
 @Document("wx_user")
 public class WxUser extends BaseModel {
     @Id
+    String id;
     String openid;
     String province;
-    String unionId;
     String country;
     String city;
-    String gender;
+    int gender;
+    String language;
     String avatarUrl;
     String nickName;
+
+    public WxUser() {
+    }
 
     public WxUser(String openid) {
         this.openid = openid;
